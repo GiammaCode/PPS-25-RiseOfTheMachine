22decb24be78313e86e1deb183e3bbde343ce4a4
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2025-04-18 16:02:56 +0200

    add comment about HumanStrategy

---
+++ src/main/scala/model/strategy/HumanStrategy.scala
@@ -0,0 +1,10 @@
+/*
+Analizzare lo stato del mondo (WorldState)
+
+Decidere dove e come difendere
+
+Decidere se e quanto avanzare il Kill Switch
+
+Restituire un'Action da eseguire (es. DefendCityAction, DevelopKillSwitchAction)
+
+ */
