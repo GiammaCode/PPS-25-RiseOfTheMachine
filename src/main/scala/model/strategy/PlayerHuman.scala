f6bdd45dd856b027cf9a1c563f5be59df3353288
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2025-04-24 15:36:00 +0200

    developed PlayerHuman

--- src/main/scala/model/strategy/PlayerHuman.scala
+++ src/main/scala/model/strategy/PlayerHuman.scala
@@ -11,8 +11,26 @@ case class PlayerHuman(
 
   override type ValidAction = HumanAction
 
-  override def executeAction(action: ValidAction): PlayerHuman = doExecuteAction(action)
+  override def executeAction(action: ValidAction): PlayerHuman =
 
-  private def doExecuteAction(action: HumanAction): PlayerHuman = ???
+    doExecuteAction(action)
 
+  private def doExecuteAction(action: HumanAction): PlayerHuman = action match
+    case action: CityDefenseAction => singleCityDefense(action.targets)
+    case action: GlobalDefenseAction => globalDefense(action.targets)
+    case action: DevelopKillSwitchAction => developKillSwitchAction
+
+
+  private def singleCityDefense(city: List[String]) : PlayerHuman =
+    copy(defendedCities = defendedCities ++ city)
+      .addAction(CityDefenseAction(city))
+
+  private def globalDefense(cities: List[String]) =
+    copy(defendedCities = defendedCities ++ cities)
+      .addAction(GlobalDefenseAction(cities))
+
+  private def developKillSwitchAction = addAction(DevelopKillSwitchAction())
+
+  private def addAction(action: HumanAction): PlayerHuman =
+    copy(executedActions = action :: executedActions)
 
