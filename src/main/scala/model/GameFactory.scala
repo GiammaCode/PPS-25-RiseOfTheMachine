6c1bc17e53436c7bf8f75217b201f69fd0abe1b5
LoryBug <72506393+LoryBug@users.noreply.github.com>
2025-04-24 17:41:41 +0200

    wip: gamefactory test

--- src/main/scala/model/GameFactory.scala
+++ src/main/scala/model/GameFactory.scala
@@ -1,5 +1,12 @@
 package model
 
-class GameFactory {
+import model.map.WorldMapModule.{UndeterministicMapModule, WorldMap, createWorldMap}
+import model.map.WorldStateModule.WorldState
+import model.strategy.{PlayerAI, PlayerHuman}
 
-}
+object GameFactory : 
+  def createGame() : (PlayerAI, PlayerHuman, WorldMap) =
+    (PlayerAI() , PlayerHuman(), createWorldMap(10)(UndeterministicMapModule)) //TODO: add creation method for player in common trait
+    
+    
+
