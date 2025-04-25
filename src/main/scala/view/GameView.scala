4794a3d7f5d225bb4421f3e39e3bad16a5554d81
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2025-04-25 11:56:53 +0200

    refact view architecture

--- src/main/scala/view/GameView.scala
+++
@@ -1,8 +0,0 @@
-package view
-
-trait GameView:
-  def renderTurn(turn: Int): String
-  def renderMap(worldMap: Set[(String, Set[(Int, Int)])], width: Int, height: Int): String
-  def renderStatus(infectedCity: Int, totalCity: Int, abilities: List[String]): String
-  def renderActionMenu(options: List[String]): String
-
