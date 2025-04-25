6d4284ba1c4b470f622818ac13a7c86dc8563287
Luca Pasini <luca.pasini8@studio.unibo.it>
2025-04-25 13:21:44 +0200

    change city name with letter

--- src/main/scala/controller/GameController.scala
+++ src/main/scala/controller/GameController.scala
@@ -41,10 +41,6 @@ case class GameState(ai: PlayerAI,
       yield()
 
   def startGame() : Unit = ???
-  /**
-   * Execute a full turn
-   * @return the updated controller
-   */
-  def executeTurn(): Unit = ???
+
 
 
--- src/main/scala/model/util/Percentage.scala
+++
@@ -1,9 +0,0 @@
-package model.util
-
-import scala.util.Random
-
-object Percentage:
-   def doesAttackWorks(percentage: Int): Boolean =
-    Random.nextInt(100) > percentage
-
- 
