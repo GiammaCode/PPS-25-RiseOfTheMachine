4dac2da30d372b24c92c52e094ce646de2f1a78b
LoryBug <72506393+LoryBug@users.noreply.github.com>
2025-05-02 14:33:34 +0200

    add get possibleAction

--- src/main/scala/model/strategy/PlayerAI.scala
+++ src/main/scala/model/strategy/PlayerAI.scala
@@ -19,7 +19,7 @@ trait PlayerAI extends PlayerEntity:
   def sabotagePower: Int
   def conqueredCities: Set[String]
   def sabotagedCities: Set[String]
-  def getPossibleAction(worldMap: WorldMap): Set[AiAction]
+  def getPossibleAction: Set[AiAction]
   override type ValidAction = AiAction
   override type Self = PlayerAI
 
@@ -51,7 +51,7 @@ private case class PlayerAIImpl (
     case Sabotage(targets) => sabotage(targets, worldMap)
     case Evolve => evolve
 
-  override def getPossibleAction(worldMap: WorldMap): Set[AiAction] =
+  override def getPossibleAction: Set[AiAction] =
     Set(Infect(), Sabotage(), Evolve)
 
   /** String representation of the AI's current status. */
