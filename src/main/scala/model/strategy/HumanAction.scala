e67af1cebfdfe131d135e3678b55ad35e20e2c70
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2025-05-10 12:00:33 +0200

    refact HumanAciton

--- src/main/scala/model/strategy/HumanAction.scala
+++ src/main/scala/model/strategy/HumanAction.scala
@@ -3,35 +3,24 @@ package model.strategy
 /** Represents actions specifically for human players */
 sealed trait HumanAction extends TurnAction:
   def targets: List[ActionTarget]
-/*
+
   def name: String = this match
     case CityDefense(_) => "CityDefense"
     case GlobalDefense(_) => "GlobalDefense"
-    case DevelopKillSwitch(_) => "DevelopKillSwitch"
+    case DevelopKillSwitch => "DevelopKillSwitch"
 
-*/
-/*
-  def execute: Unit
 
 object HumanAction:
-  def cityDefense(targets: List[ActionTarget]): HumanAction = CityDefense(targets)
-  def globalDefense(targets: List[ActionTarget]): HumanAction = GlobalDefense(targets)
+  val allActions: List[HumanAction] = List(CityDefense(), GlobalDefense(), DevelopKillSwitch)
+  def cityDefense(targets: List[ActionTarget]) : HumanAction = CityDefense(targets)
+
+  def globalDefense(): HumanAction = GlobalDefense()
+
   def developKillSwitch(): HumanAction = DevelopKillSwitch
 
-/** Concrete implementation: CityDefense */
-case class CityDefense(targets: List[ActionTarget] = List.empty) extends HumanAction:
-  override def execute: Unit =
-    if (targets.isEmpty) println("City Defense failed: No targets specified")
-    else println(s"City Defense activated on: ${targets.mkString(", ")}")
+case class CityDefense(targets: List[ActionTarget] = List.empty) extends HumanAction
 
-/** Concrete implementation: GlobalDefense */
-case class GlobalDefense(targets: List[ActionTarget]= List.empty) extends HumanAction:
-  override def execute: Unit =
-    if (targets.isEmpty) println("Global Defense failed: No targets specified")
-    else println(s"Global Defense deployed to: ${targets.mkString(", ")}")
+case class GlobalDefense(targets: List[ActionTarget] = List.empty) extends HumanAction
 
-/** Concrete implementation: DevelopKillSwitch (no targets) */
 case object DevelopKillSwitch extends HumanAction:
   override def targets: List[ActionTarget] = List.empty
-  override def execute: Unit = println("Kill Switch developed successfully")
-*/
