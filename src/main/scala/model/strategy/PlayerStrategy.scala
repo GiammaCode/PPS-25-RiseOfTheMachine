391b6cc18eb122e75227e5649e60b263e4e04a87
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2025-05-20 16:13:35 +0200

    add method probabilieties by city and action

--- src/main/scala/model/strategy/PlayerStrategy.scala
+++ src/main/scala/model/strategy/PlayerStrategy.scala
@@ -12,14 +12,53 @@ trait PlayerStrategy[A <: TurnAction]:
 object SmartHumanStrategy extends PlayerStrategy[HumanAction]:
 
   override def decideAction(state: WorldState): HumanAction =
-    val possibleTargets = state.attackableCities.map(_._1).toList
-    val cityToDefend = possibleTargets
-      .filterNot(state.playerHuman.defendedCities.contains)
-      .take(1)
 
-    val actions = List(
-      Option.when(cityToDefend.nonEmpty)(CityDefense(cityToDefend)),
-      Some(GlobalDefense())
-    ).flatten
+    //strategia passiva
+    def decideEasy: HumanAction =
+      val possibleTargets = state.attackableCities.map(_._1).toList
+      val cityToDefend = possibleTargets
+        .filterNot(state.playerHuman.defendedCities.contains)
+        .take(1)
 
-    Random.shuffle(actions).head
+      val actions = List(
+        Option.when(cityToDefend.nonEmpty)(CityDefense(cityToDefend)),
+        Some(GlobalDefense())
+      ).flatten
+
+      Random.shuffle(actions).head
+
+    //strategia normale random su tutte le azioni
+    def decideNormal: HumanAction =
+      val possibleTargets = state.attackableCities.map(_._1).toList
+      val cityToDefend = possibleTargets
+        .filterNot(state.playerHuman.defendedCities.contains)
+        .take(1)
+
+      val actions = List(
+        Option.when(cityToDefend.nonEmpty)(CityDefense(cityToDefend)),
+        Option.when(state.playerHuman.killSwitch < 50)(DevelopKillSwitch),
+        Some(GlobalDefense())
+      ).flatten
+      Random.shuffle(actions).head
+
+    //proactive strategy
+    def decideHard: HumanAction =
+      val possibleTargets = state.attackableCities.toList
+        .sortBy { case (_, infect, sabotage) => -(infect + sabotage) } // prioritize higher risk
+        .map(_._1)
+
+      val topCities = possibleTargets
+        .filterNot(state.playerHuman.defendedCities.contains)
+        .take(2)
+
+      val actions = List(
+        Option.when(topCities.nonEmpty)(CityDefense(topCities)),
+        Option.when(state.playerHuman.killSwitch < 80)(DevelopKillSwitch),
+        Some(GlobalDefense())
+      ).flatten
+      Random.shuffle(actions).head
+
+    state.difficulty match
+      case Difficulty.Easy => decideEasy
+      case Difficulty.Normal => decideNormal
+      case Difficulty.Hard => decideHard
