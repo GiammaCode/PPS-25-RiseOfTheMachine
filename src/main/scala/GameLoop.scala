804cf30fda26499596aaf0aae5ee38c8e0c6d356
Luca Pasini <luca.pasini8@studio.unibo.it>
2025-05-22 13:24:27 +0200

    refactor game loop

--- src/main/scala/GameLoop.scala
+++ src/main/scala/GameLoop.scala
@@ -1,19 +1,15 @@
 import controller.GameController
-import controller.GameController.{GameState, buildGameState, gameTurn}
-import jdk.internal.classfile.impl.AbstractInstruction.UnboundStackInstruction
+import controller.GameController.{GameState, gameTurn}
 import model.util.GameSettings.*
-import view.ViewModule.CLIView
 
 import scala.annotation.tailrec
 object StartGameLoop
 def GameLoop(initialState: GameState)(using gameState: GameSettings): Unit =
-  val maxTurn= 15
   @tailrec
-  def loop(turn: Int, state: GameState)(using gameSettings: GameSettings): GameState =
-    if turn > maxTurn || state.isGameOver then state
-    else//println(s"\n--- Turno $turn ---")
+  def loop(state: GameState)(using gameSettings: GameSettings): GameState =
+    if state.isGameOver then state
+    else
       val (newState, _) = gameTurn.run(state)
-      loop(turn + 1, newState)
-  val finalState = loop(1, initialState)
-  println("\n--- Fine partita (raggiunto limite massimo di turni) ---")
+      loop(newState)
+  val finalState = loop(initialState)
 
