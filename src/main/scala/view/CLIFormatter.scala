9193cd06c4a5ed633f974125da26a3937cd0aced
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2025-05-29 18:22:54 +0200

    create CliFormatter utils

---
+++ src/main/scala/view/CLIFormatter.scala
@@ -0,0 +1,8 @@
+package view
+
+object CLIFormatter:
+  def printBoxedContent(title: String, body: List[String]): Unit  = ???
+  def printBoxedMenu(title: String, body: List[String]): Unit  = ???
+  def printAsciiTitle(title: String): Unit = ???
+  def printMessageBlock(body: List[String]) = ???
+
